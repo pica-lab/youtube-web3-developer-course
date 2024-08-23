45f5a81bc9335d936530f38ad8716ebf6ab3a860
picalabsuperadmin <superadmin@picalab.ai>
2024-08-23 20:34:36 +0800

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,24 +2,24 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <div className="flex flex-col items-center mt-20">
-            <Link
-                className="text-center text-sm text-white-400 bg-red-500 px-4 py-2 rounded-md"
-                target="_blank"
-                href="https://www.youtube.com/playlist?list=PLhkjr9MPgk0yHNoVZqpWpBg9s029TAFPE"
-            >
-                Watch tutorial on YouTube
-            </Link>
-            <Link
-                className="text-center text-sm text-gray-400 mt-2"
-                target="_blank"
-                href="https://github.com/thirdweb-example/youtube-web3-developer-course"
-            >
-                View code on GitHub
-            </Link>
-            <Link href={"/"} className="text-sm text-gray-400 mt-8">
-                Back to menu
-            </Link>
-        </div>
+        <footer className="flex flex-col items-center mt-20 py-10 bg-gray-800 text-gray-300">
+            <div className="flex flex-row space-x-4 mb-4">
+                <Link href="https://octopus.com" passHref>
+                    <a className="hover:underline">Octopus Deploy</a>
+                </Link>
+                <Link href="https://docs.octopus.network" passHref>
+                    <a className="hover:underline">Documentation</a>
+                </Link>
+                <Link href="https://blog.octopus.network" passHref>
+                    <a className="hover:underline">Blog</a>
+                </Link>
+                <Link href="https://community.octopus.network" passHref>
+                    <a className="hover:underline">Community</a>
+                </Link>
+            </div>
+            <div className="text-sm">
+                &copy; {new Date().getFullYear()} Octopus X . All rights reserved.
+            </div>
+        </footer>
     )
 }
