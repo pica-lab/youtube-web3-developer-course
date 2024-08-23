efef0028fe30fc41bc8501b26e4fc17265c9d3ff
picalabsuperadmin <superadmin@picalab.ai>
2024-08-23 21:26:14 +0800

    Update page.tsx

--- src/app/connect-embed/page.tsx
+++ src/app/connect-embed/page.tsx
@@ -7,7 +7,7 @@ import { createWallet } from "thirdweb/wallets";
 
 const ConnectEmbedPage: React.FC = () => {
     return (
-        <div className="px-4 md:px-10 lg:px-20">
+        <div className="container">
             <Header 
                 title="ConnectEmbed UI Component"
                 subtitle="Learn what our ConnectEmbed UI component is, how to use it, and how to customize it."
